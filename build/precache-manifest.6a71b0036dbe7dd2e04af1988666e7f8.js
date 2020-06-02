9804eeb963ddbd33ff2b8d980cc81a752b22c16c
Christopher Coles <chriscoles@ChristophersMBP.home>
2020-06-02 15:08:43 -0700

    Initial commit. Work on new 2020 portfolio site.

---
+++ build/precache-manifest.6a71b0036dbe7dd2e04af1988666e7f8.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3d45e55df37742925e9d5b973f566cfd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5dcc10720ed9fac6421",
+    "url": "/static/css/2.f7be325d.chunk.css"
+  },
+  {
+    "revision": "c6e21e4ab9bb6bc3e8bf",
+    "url": "/static/css/main.03d577c2.chunk.css"
+  },
+  {
+    "revision": "f5dcc10720ed9fac6421",
+    "url": "/static/js/2.90476baa.chunk.js"
+  },
+  {
+    "revision": "c6e21e4ab9bb6bc3e8bf",
+    "url": "/static/js/main.6886d7ec.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
