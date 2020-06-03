7bff9294f8004bfb046e295f68dc16965a3a4041
Christopher Coles <chriscoles@ChristophersMBP.home>
2020-06-03 14:24:29 -0700

    Updates

---
+++ precache-manifest.7d9104904dbb111917938414e5bd7b12.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c373882695354fc9379c70c04f8dc9e9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "45774ab3f72c5c16c153",
+    "url": "/static/css/2.0cc4c28a.chunk.css"
+  },
+  {
+    "revision": "eff25a7135b36b61ab58",
+    "url": "/static/css/main.77853703.chunk.css"
+  },
+  {
+    "revision": "45774ab3f72c5c16c153",
+    "url": "/static/js/2.80dd74c2.chunk.js"
+  },
+  {
+    "revision": "eff25a7135b36b61ab58",
+    "url": "/static/js/main.66f6a6ee.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
